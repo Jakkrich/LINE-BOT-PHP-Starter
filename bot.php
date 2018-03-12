88adf504fe814f3ca3ffba81107afa36f42ba0aa
jakkrich changgon <mr.jakrich@gmail.com>
2018-03-12 20:50:12 +0700

    Demo

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
-define("LINE_MESSAGING_API_CHANNEL_SECRET", "HcFHTh85/WUrDeR85X7MjkKHK1QLkyadxIYs0UCCZ+D7/DmdGd0f5HrWslIOeS/9ONeSJK5XmKzZPOIRwB7usy99mRvB8z8dj5X0OV6xBWFSYErp2zdZgejqTT5T/zcbZZj/EQ5wxfxKnz4WvM7UMwdB04t89/1O/w1cDnyilFU=");
-define("LINE_MESSAGING_API_CHANNEL_TOKEN", "2e54360ce915aeb46e95c54cf392d685");
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", "HcFHTh85/WUrDeR85X7MjkKHK1QLkyadxIYs0UCCZ+D7/DmdGd0f5HrWslIOeS/9ONeSJK5XmKzZPOIRwB7usy99mRvB8z8dj5X0OV6xBWFSYErp2zdZgejqTT5T/zcbZZj/EQ5wxfxKnz4WvM7UMwdB04t89/1O/w1cDnyilFU=");
+define("LINE_MESSAGING_API_CHANNEL_SECRET", "2e54360ce915aeb46e95c54cf392d685");
 
 require __DIR__."/../vendor/autoload.php";
-
+/*
 $bot = new \LINE\LINEBot(
     new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
     ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
@@ -21,5 +21,5 @@ foreach ($events as $event) {
         $bot->replyText($reply_token, $text);
     }
 }
-
+*/
 echo "OK";
