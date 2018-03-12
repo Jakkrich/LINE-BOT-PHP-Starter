febba29744e9379776f7b512ff203c24c67fc354
jakkrich changgon <mr.jakrich@gmail.com>
2018-03-12 21:22:10 +0700

    demo

--- bot.php
+++ bot.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define("LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN", "HcFHTh85/WUrDeR85X7MjkKHK1QLkyadxIYs0UCCZ+D7/DmdGd0f5HrWslIOeS/9ONeSJK5XmKzZPOIRwB7usy99mRvB8z8dj5X0OV6xBWFSYErp2zdZgejqTT5T/zcbZZj/EQ5wxfxKnz4WvM7UMwdB04t89/1O/w1cDnyilFU=");
 define("LINE_MESSAGING_API_CHANNEL_SECRET", "2e54360ce915aeb46e95c54cf392d685");
 
