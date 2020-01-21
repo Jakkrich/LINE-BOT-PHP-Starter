fa144b4927382c0e135e92f95344067626e9e5e4
Jakkrich Changgon <jakkrich.changgon@nstda.or.th>
2020-01-21 14:26:53 +0700

    test

--- bot_settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID', 'xxx');
-define('LINE_MESSAGE_CHANNEL_SECRET', 'xxx');
-define('LINE_MESSAGE_ACCESS_TOKEN', 'xxx');
-?>
