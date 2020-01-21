287a39ffccd22034bfc24d581821b02cf6f44fc4
Jakkrich Changgon <jakkrich.changgon@nstda.or.th>
2020-01-21 14:37:20 +0700

    test

---
+++ xo/js/menuHandler.js
@@ -0,0 +1,32 @@
+const modeSpan = document.querySelector('.mode span');
+
+document.querySelector('.pvp').addEventListener('click', e => {
+  document.querySelector('#menu-toggle').checked = !document.querySelector('#menu-toggle').checked;
+  reset();
+  challengeBot = false;
+  modeSpan.textContent = "Player";
+});
+
+document.querySelector('.pvcEasy').addEventListener('click', e => {
+  document.querySelector('#menu-toggle').checked = !document.querySelector('#menu-toggle').checked;
+  reset();
+  challengeBot = true;
+  difficulty = "easy";
+  modeSpan.textContent = "Computer (Easy)";
+});
+
+document.querySelector('.pvcHard').addEventListener('click', e => {
+  document.querySelector('#menu-toggle').checked = !document.querySelector('#menu-toggle').checked;
+  reset();
+  challengeBot = true;
+  difficulty = "hard";
+  modeSpan.textContent = "Computer (Hard)";
+});
+
+document.querySelector('.pvcAi').addEventListener('click', e => {
+  document.querySelector('#menu-toggle').checked = !document.querySelector('#menu-toggle').checked;
+  reset();
+  challengeBot = true;
+  difficulty = "ai";
+  modeSpan.textContent = "Computer (AI)";
+});
